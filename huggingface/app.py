7f1905c4f39ef26141643c3264ba9ac1f765e8c3
torileatherman <113507754+torileatherman@users.noreply.github.com>
2022-12-09 14:48:20 +0100

    Update app.py

--- huggingface/app.py
+++ huggingface/app.py
@@ -1,18 +1,48 @@
 from transformers import pipeline
 import gradio as gr
+import os
+import deepl
+import openai
+from pytube import YouTube
 
-pipe = pipeline(model="torileatherman/train_first_try")  # change to "your-username/the-name-you-picked"
+TARGET_LANG = "EN-GB"
+deepl_key = os.environ.get('DEEPL_KEY')
+
+translator = deepl.Translator(deepl_key)
+pipe = pipeline(model="torileatherman/whisper_small_sv")  # change to "your-username/the-name-you-picked"
 
 def transcribe(audio):
-    text = pipe(audio)["text"]
-    return text
+    text_sv = pipe(audio)["text"]
+    print(f"Audio transcribed: {text_sv}")
+    text_en = translator.translate_text(text_sv, target_lang=TARGET_LANG).text
+    print(f"Text translated: {text_en}")
+    return text_sv, text_en
 
-iface = gr.Interface(
-    fn=transcribe, 
-    inputs=gr.Audio(source="microphone", type="filepath"), 
-    outputs="text",
-    title="Whisper Small Swedish",
-    description="Realtime demo for Swedish speech recognition using a fine-tuned Whisper small model.",
+def transcribe_url(url):
+    youtube = YouTube(str(url))
+    audio = youtube.streams.filter(only_audio=True).first().download('yt_video')
+    text_sv = pipe(audio)["text"]
+    text_en = translator.translate_text(text_sv, target_lang=TARGET_LANG).text
+    return text_sv, text_en
+
+url_demo = gr.Interface(
+    fn=transcribe_url, 
+    inputs="text", 
+    outputs=[gr.Textbox(label="Transcribed text"),
+             gr.Textbox(label="English translation")],
+    title="Swedish video speech to english text",
+    description="Transcribing swedish video to text and translating to english!",
 )
 
-iface.launch()
+voice_demo = gr.Interface(
+    fn=transcribe, 
+    inputs=gr.Audio(source="microphone", type="filepath"), 
+    outputs=[gr.Textbox(label="Transcribed text"),
+             gr.Textbox(label="English translation")],
+    title="Swedish recorded speech to english text",
+    description="Transcribing swedish speech to text and translating to english!",
+)
+
+demo = gr.TabbedInterface([url_demo, voice_demo], ["Swedish YouTube Video to English Text", "Swedish Audio to English Text"])
+
+demo.launch()
